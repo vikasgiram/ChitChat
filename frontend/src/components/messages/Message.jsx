692fb2d6cf7a98bdc419c53542f4fc658ec8ff13
Vicky <vikigiram@gmail.com>
2024-04-27 17:18:10 +0530

    added search functionlity

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext';
+import useConversation from '../../zustand/useConversation';
+import {extractTime} from '../../utils/extractTime'
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser}=useAuthContext();
+  const {selectedConversation}= useConversation();
+  const fromMe =message.senderId === authUser._id;
+  const chatClassName= fromMe ? 'chat-end' : 'chat-start';
+  const profilePic= fromMe ? authUser.profilePic: selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500': '';
+  const formatedTime= extractTime(message.createdAt);
+
   return (
-    <div className='chat chat-end '>
+    <div className={`chat ${chatClassName}`}>
         <div className='avatar chat-image'>
             <div className='w-10 rounded-full'>
-                <img src="https://avatar.iran.liara.run/public/boy?username=vickygiram" alt="chat bubble" />
+                <img src={profilePic} alt="chat bubble" />
             </div>
         </div>
       
-      <div className={`chat-bubble text-white bg-blue-500 `}> Hi! what is upp?</div>
-      <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>12.42</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pd-2 `}>{message.message}</div>
+      <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>{formatedTime }</div>
     </div>
   );
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message';
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeletons/MessageSkeletons"
 
 const Messages = () => {
   const {messages, loading}=useGetMessages();
-  console.log("Messages:",messages);
+  const lastMessageRef = useRef();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+    },100);
+  },[messages]);
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
+        {!loading &&
+          messages.length > 0 && 
+          messages.map((message)=> (
+            <div key={message._id} ref={lastMessageRef}>
+              <Message message={message} />
+            </div>
+          ))
+        }
+
         {loading && [...Array(3)].map((_, idx)=> < MessageSkeleton key={idx} />)}
 
         {!loading && messages.length === 0  && (
