e21d31407e0fdc18c9c0e1ce8615f8cf1d29b2c9
Vicky <vikigiram@gmail.com>
2024-04-27 17:18:43 +0530

    fix bugs

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -41,18 +41,19 @@ export const sendMessage= async(req,res)=>{
     }
 }
 
-export const getMessages= async(req,res)=>{
+export const getMessages= async (req, res)=>{
     try{
 
-        const {id:userToChatId}=req.params;
+        const { id: userToChatId }=req.params;
         const senderId=req.user._id;
 
         const conversation= await Convervation.findOne({
-            participants:{$all:[senderId, userToChatId]},
+            participants:{ $all: [senderId, userToChatId] },
         }).populate("message"); // reference but actual messages
         
+        
         if(!conversation){
-            res.status(200).json([]);
+            return res.status(200).json([]);
         }
 
         const messages=conversation.message;
--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -15,7 +15,7 @@ const messageSchema = new mongoose.Schema({
     message:{
         type: String,
         required: true
-    }
+    },
 }, {timestamps:true});
 
 const Message=mongoose.model("Message", messageSchema);
